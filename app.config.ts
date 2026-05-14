942a49f065a8b7d220d0622c7cd72478c5900dfe
armantorkzaban <armantorkzaban@icloud.com>
2026-05-14 19:45:52 +0200

    fix: standardize import statements with consistent semicolons

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
-import type { ConfigContext, ExpoConfig } from '@expo/config'
+import type { ConfigContext, ExpoConfig } from '@expo/config';
 
-import { ClientEnv, Env } from './env'
+import { ClientEnv, Env } from './env';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
